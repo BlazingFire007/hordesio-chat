6e1eda44d7bf83e426de021372650f1231cfeb84
BlazingFire007 <elijahrich02@icloud.com>
2018-02-13 17:47:48 -0600

    Fixed bugs

--- chat.js
+++ chat.js
@@ -5,13 +5,14 @@ let socket = io.connect("https://hordes.io:443");
 socket.on("c", log);
 
 
-function log({n, m, c, i, r}) {
+function log({n, m, c, i, r, f}) {
     let message = '';
     if (c) c += " ";
     else c = '';
-    if (r === "1") c = chalk.red(c);
+    if (f === 1) c = chalk.redBright(c);
     else c = chalk.blueBright(c);
     if (i === "gm") m = chalk.bgBlue(m);
     message += `[${c}${n}]: ${m}`;
+    if (r === 0) message = chalk.gray(message);
     console.log(message);
 }
